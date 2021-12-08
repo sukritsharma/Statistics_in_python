237a348b241d591ca02eda689b1a60144c68913f
sukritsharma <sukritsharma7@gmail.com>
2021-12-08 12:27:40 +0100

    <add python and cvs files>

---
+++ Run_all.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 22 18:55:02 2021
+
+@author: Sukrit Sharma
+"""
+
+# import single_patient_voxel_table
+# import combine_patients_and_post_processing
+# import corelation_matrix
+# import Boxplots_all_type
+
+# import Recursive_feature_selection_multiple_threshold
+# import RF_without_feature_selection_multiple_cutoff
+# import prediction_analysis
+
+# import Recursive_feature_selection_multiple_threshold_grade
+# import RF_without_feature_selection_multiple_cutoff_grade
+# import prediction_analysis_grade
+
+
+
+import Recursive_feature_selection_multiple_threshold_v01
+# import RF_without_feature_selection_multiple_cutoff_v01
+import prediction_analysis_v01
+
+
+import Recursive_feature_selection_multiple_threshold_grade_v01
+import RF_without_feature_selection_multiple_cutoff_grade_v01
+import prediction_analysis_grade_v01
+
+# import RF_without_feature_selection_multiple_cutoff_grade
+# import RF_without_feature_selection_multiple_cutoff_grade_v01
+# import prediction_analysis_grade
+# import prediction_analysis_grade_v01
